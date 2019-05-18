a0b253d2c4931c205fe66eaef2db3b4feb4b6c23
Derek Miller <derekjmiller@gmail.com>
2019-05-17 17:48:01 -0700

    Begin adding unit test

---
+++ trie/tests/trie_test.py
@@ -0,0 +1,8 @@
+import Trie
+import unittest
+
+class Test_TrieEnumerate(unittest.TestCase):
+    trie = Trie('C:\\Users\\derek\\Documents\\repos\\tinfo498\\test\\wordlist.txt')
+    print(trie.enumerate())
+    
+
